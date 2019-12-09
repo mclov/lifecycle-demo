8d7e977bcc6068c771611a5cb07c3d65d863bf96
Michael Clovis <michaelclovis@protonmail.com>
2019-12-09 12:59:26 -0800

    add user info

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <GithubUserInfo username="Elie"/>
+        <GithubUserInfo username="mclov" />
       </div>
     );
   }
